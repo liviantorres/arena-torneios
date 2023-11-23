7a1b2d96c29b15fdec8cd3125f52fbe2cd029cc2
Laércio Levi <levimelos200@gmail.com>
2023-11-23 13:47:18 -0300

    Algumas telas
    Foram feitas mais algumas telas, porém não estão organizadas

---
+++ Front-end/tela-jogador/funcoes.js
@@ -0,0 +1,15 @@
+document.getElementById('criarcampeonato').addEventListener('click', function() {
+    document.getElementById('overlay').style.display = 'flex';
+  });
+
+  document.getElementById('janelafechar').addEventListener('click', function() {
+    document.getElementById('overlay').style.display = 'none';
+  });
+
+  document.getElementById('timesad').addEventListener('click', function() {
+    document.getElementById('overlay3').style.display = 'flex';
+  });
+
+  document.getElementById('janelafechar3').addEventListener('click', function() {
+    document.getElementById('overlay3').style.display = 'none';
+  });
---
+++ Front-end/tela_campeonato/funcoes2.js
@@ -0,0 +1,9 @@
+
+
+  document.getElementById('timesabrir').addEventListener('click', function() {
+    document.getElementById('overlay2').style.display = 'flex';
+  });
+
+  document.getElementById('janelatimeclose').addEventListener('click', function() {
+    document.getElementById('overlay2').style.display = 'none';
+  });
